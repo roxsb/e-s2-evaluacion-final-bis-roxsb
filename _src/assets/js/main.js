82c34f4390597428d8977f510f18ba83a65f96ce
roxsb <rox.san.bri@gmail.com>
2019-01-01 21:42:28 +0100

    look better

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -8,20 +8,18 @@ let radioValue = '';
 let pokemonArray = '';
 let pokemonPairActive = '';
 
-console.log(selectFormValue.value);
-
-//Al hacer clic sobre el botón de 'Comenzar', recoge el valor del tamaño de la partida
 function setRadio(e){
   for(let i = 0; i< selectFormValue.length; i++){
     selectFormValue[i].classList.remove('selected');
   }
   e.currentTarget.classList.add('selected');
   radioValue = event.currentTarget.value;
-  //guardar este número en localStorage
   localStorage.setItem('number',JSON.stringify(radioValue))
 }
+
 function getRadio () {
 }
+
 function clickListener(){
   for(let i=0; i<selectFormValue.length;i++){
     selectFormValue[i].addEventListener('click',setRadio);
@@ -29,11 +27,11 @@ function clickListener(){
 }
 buttonStart.addEventListener('click',getCards);
 clickListener();
-//mostrar la parte de atrás siempre añadiendo la clase
+
 function loadCards (data) {
   pokemonArray +=`<li class="box opposite__image"><img class ="pokemon__image" src="${data.image}"></li>`;
 }
-//conectar a una API que devuelve un listado de cartas donde NUMERO puede tomar el valor de 4, 6 y 8. obtendremosla URL de la imagen a mostrar
+
 function getCards(){
   fetch(`https://raw.githubusercontent.com/Adalab/cards-data/master/${radioValue}.json`)
     .then(function(response) {
@@ -50,7 +48,6 @@ function getCards(){
 }
 
 function activePokemon(e){
-  //Al hacer clic sobre una carta vamos a mostrar su parte frontal y a ocultar su parte trasera viendo si tiene la clase active, si no, la añade, y si la tiene se la quita
   if( !e.currentTarget.classList.contains('active__pokemon') ){
     e.currentTarget.classList.add('active__pokemon');
   }else{
@@ -59,8 +56,8 @@ function activePokemon(e){
 }
 
 function pokemonListeners(){
-  for(let i=0;i<cardsList.childNodes.length;i++){
-    cardsList.childNodes[i].addEventListener('click',activePokemon);
+  for(let i=0;i<cardsList.children.length;i++){
+    cardsList.children[i].addEventListener('click',activePokemon);
   }
 }
 
